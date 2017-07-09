a302fb74b26a97093004513687db27f0fe4e010c
teibok <smartwork00001@gmail.com>
2017-07-09 21:21:24 +0530

    form-based workflow completed. Wufff!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  include ArticlesHelper
+
   def main
   end
 
@@ -7,10 +10,36 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article=Article.new
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article=Article.new(article_params)
+    @article.save
+    flash.notice = "Article #{@article.title} created!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article #{@article.title} destroyed!"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article #{@article.title} updated!"
+    redirect_to article_path(@article)
   end
 
 end
